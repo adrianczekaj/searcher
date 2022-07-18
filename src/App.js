f51e2b5e1a63e26d36e66af1c5f18d71f055d38c
adrianczekaj <aczekaj.inf@gmail.com>
2022-07-18 11:24:44 +0200

    Reading data (persons) from json-server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from "./components/Header";
 import Search from "./components/Search";
@@ -6,131 +6,23 @@ import Persons from "./components/Persons";
 import PersonDetails from "./components/PersonDetails";
 
 function App() {
-    const [persons, setPersons] = useState(
-        [
-            {
-                id: 1,
-                name: 'Piotr',
-                gender: 'man',
-                height: 145,
-            },
-            {
-                id: 2,
-                name: 'Krzysztof',
-                gender: 'man',
-                height: 183,
-            },
-            {
-                id: 3,
-                name: 'Anna',
-                gender: 'woman',
-                height: 187,
-            },
-            {
-                id: 4,
-                name: 'Tomasz',
-                gender: 'man',
-                height: 180,
-            },
-            {
-                id: 5,
-                name: 'Paweł',
-                gender: 'man',
-                height: 180,
-            },
-            {
-                id: 6,
-                name: 'Agnieszka',
-                gender: 'woman',
-                height: 156,
-            },
-            {
-                id: 7,
-                name: 'Michał',
-                gender: 'man',
-                height: 180,
-            },
-            {
-                id: 8,
-                name: 'Marcin',
-                gender: 'man',
-                height: 170,
-            },
-            {
-                id: 9,
-                name: 'Jakub',
-                gender: 'man',
-                height: 165,
-            },
-            {
-                id: 10,
-                name: 'Adam',
-                gender: 'man',
-                height: 194,
-            },
-            {
-                id: 11,
-                name: 'Andrzej',
-                gender: 'man',
-                height: 172,
-            },
-            {
-                id: 12,
-                name: 'Katarzyna',
-                gender: 'woman',
-                height: 180,
-            },
-            {
-                id: 13,
-                name: 'Maria',
-                gender: 'woman',
-                height: 170,
-            },
-            {
-                id: 14,
-                name: 'Małgorzata',
-                gender: 'woman',
-                height: 143,
-            },
-            {
-                id: 15,
-                name: 'Jan',
-                gender: 'man',
-                height: 168,
-            },
-            {
-                id: 16,
-                name: 'Barbara',
-                gender: 'woman',
-                height: 143,
-            },
-            {
-                id: 17,
-                name: 'Ewa',
-                gender: 'woman',
-                height: 167,
-            },
-            {
-                id: 18,
-                name: 'Magdalena',
-                gender: 'woman',
-                height: 175,
-            },
-            {
-                id: 19,
-                name: 'Elżbieta',
-                gender: 'woman',
-                height: 172,
-            },
-            {
-                id: 20,
-                name: 'Krystyna',
-                gender: 'woman',
-                height: 173,
-            }
-        ]);
+    const [persons, setPersons] = useState([]);
 
     const [showSearchPersons, setShowSearchPerson] = useState(false);
+
+    useEffect(()=>{
+        const getPersons = async () => {
+            const personsFromServer = await fetchPersons()
+            setPersons(personsFromServer)
+        }
+
+        getPersons()
+    }, [])
+
+    const fetchPersons = async () => {
+        const res = await fetch('http://localhost:5000/persons')
+        return await res.json()
+    }
 
     return (
         <Router>
