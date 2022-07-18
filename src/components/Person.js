a42edd387f58b6192ee3b25f82a12a658d6a4d94
adrianczekaj <aczekaj.inf@gmail.com>
2022-07-18 11:44:33 +0200

    Printing keys of the persons has been changed into printing next numbers

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 
-const Person = ({person}) => {
+const Person = ({person, number}) => {
     const {id, name, gender, height} = person;
 
     return (
         <div className="person">
-            <div className="id">{id}</div>
+            <div className="number">{number+1}</div>
             <div className="name">{name}</div>
             <div className="gender">{gender}</div>
             <div className="height">{height}</div>
--- src/components/Persons.js
+++ src/components/Persons.js
@@ -3,7 +3,7 @@ import Person from "./Person";
 const Persons = ({persons}) => {
     return (
         <div>
-            {persons.map((person) => (<Person key={person.id} person={person}/>))}
+            {persons.map((person, index) => (<Person key={person.id} number={index} person={person}/>))}
         </div>
     );
 };
